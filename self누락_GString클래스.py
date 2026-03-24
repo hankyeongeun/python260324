58f084ab13dec3b9ef9af54030d62779b9ffd10e
kyeong-eun <gumnacool@gmail.com>
2026-03-24 13:44:34 +0900

    클래스연습완료

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,3 +1,4 @@
+
 strName = "Not Class Member"
 
 class DemoString:
@@ -6,7 +7,7 @@ class DemoString:
     def set(self, msg):
         self.strName = msg
     def print(self):
-        print(strName)
+        print(self.strName)
 
 d = DemoString()
 d.set("First Message")
